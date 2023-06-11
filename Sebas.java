8f850eecdb77ed949423d4abb90abf3d6b528d59
Sebastian Tituana <satituana@uce.edu.ec>
2023-06-11 15:25:00 -0500

    Commit inicial

---
+++ Sebas.java
@@ -0,0 +1,7 @@
+public class Sebas {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+
+
+}
---
+++ archivo2.java
@@ -0,0 +1,26 @@
+public class archivo2 {
+    private String nombre;
+    private String tamanio;
+
+
+    public archivo2(String nombre, String tamanio) {
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+}
